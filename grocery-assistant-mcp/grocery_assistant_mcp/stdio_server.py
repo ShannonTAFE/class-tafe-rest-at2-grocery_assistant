896dca36fff767f0c78b94aef7ce2ce164bd2a19
shannon20075186 <20075186@tafe.wa.edu.au>
2026-05-11 14:30:58 +0800

    tests paths and csv data

--- grocery-assistant-mcp/grocery_assistant_mcp/stdio_server.py
+++ grocery-assistant-mcp/grocery_assistant_mcp/stdio_server.py
@@ -3,7 +3,7 @@ from mcp.server.fastmcp import FastMCP
 from grocery_assistant_mcp.mcp_resources.register import register_resources
 #from grocery_assistant_mcp.mcp_tools.register import register_tools
 #from grocery_assistant_mcp.mcp_prompts.register import register_prompts
-#from grocery_assistant_mcp.utils.logging_config import configure_logging
+from grocery_assistant_mcp.utils.logging_config import configure_logging
 
 
 def create_mcp_server() -> FastMCP:
